e6111fdd0b8476e3fd8f23ee1433e663774370c5
gujoh <gustafjohanson@live.com>
2021-09-17 11:42:37 +0200

    Added main class

---
+++ src/main/java/com/zcrabblers/zcrabble/Main.java
@@ -0,0 +1,19 @@
+package com.zcrabblers.zcrabble;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("board.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
