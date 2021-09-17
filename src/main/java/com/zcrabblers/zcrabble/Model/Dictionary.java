e07f6769a3c07facbcafe079aea5a15b3a912b55
martin <martinbcd@live.se>
2021-09-17 11:50:44 +0200

    reupload old dictionary

--- src/main/java/com/zcrabblers/zcrabble/Model/Dictionary.java
+++ src/main/java/com/zcrabblers/zcrabble/Model/Dictionary.java
@@ -1,5 +1,9 @@
 package com.zcrabblers.zcrabble.Model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Dictionary {
 
     private static Dictionary instance;
@@ -9,6 +13,21 @@ public class Dictionary {
     }
 
     public static Dictionary getInstance(){
+        if (instance == null){
+            return new Dictionary();
+        }
         return instance;
+    }
+
+    public boolean checkWord(String word) throws FileNotFoundException {
+        File file = new File("Collins Scrabble Words 2019");
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            if(word.toUpperCase().equals(line)){
+                return true;
+            }
+        }
+        return false;
     }
 }
