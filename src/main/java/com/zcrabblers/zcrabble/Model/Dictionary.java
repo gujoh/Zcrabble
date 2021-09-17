de4c4b9c1a80eaa022569167b56d7d3e03dda51b
Ole <ole.fjeldsaa@live.com>
2021-09-17 12:44:35 +0200

    committing to merge master into ole

--- src/main/java/com/zcrabblers/zcrabble/Model/Dictionary.java
+++ src/main/java/com/zcrabblers/zcrabble/Model/Dictionary.java
@@ -1,6 +1,7 @@
 package com.zcrabblers.zcrabble.Model;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,11 +17,14 @@ public class Dictionary {
         return instance;
     }
 
-    private ArrayList<String> dictArray(){
+    private ArrayList<String> dictArray() throws FileNotFoundException {
         ArrayList<String> dict = new ArrayList<>();
         File collins = new File("Collins Scrabble Words 2019");
         Scanner sc = new Scanner(collins);
-        while ()
+        while (sc.hasNextLine()){
+        int =
+
+        }
 
 
 
