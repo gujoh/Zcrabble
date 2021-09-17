36e75775c197c4f7a5a48001eab2778cfc956978
Ole <ole.fjeldsaa@live.com>
2021-09-17 14:44:23 +0200

    Dictionary can now check if a word is valid

--- src/main/java/com/zcrabblers/zcrabble/Model/Dictionary.java
+++ src/main/java/com/zcrabblers/zcrabble/Model/Dictionary.java
@@ -2,7 +2,6 @@ package com.zcrabblers.zcrabble.Model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Dictionary {
@@ -16,22 +15,30 @@ public class Dictionary {
     public static Dictionary getInstance(){
         return instance;
     }
+
+    public boolean checkWord(String word) throws FileNotFoundException {
+        File file = new File("Collins Scrabble Words 2019");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (word.toUpperCase().equals(line)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     /*
 
+    Method to create a faster dictionary.will be implemented later.
 
     private ArrayList<String> dictArray() throws FileNotFoundException {
         ArrayList<String> dict = new ArrayList<>();
         File collins = new File("Collins Scrabble Words 2019");
         Scanner sc = new Scanner(collins);
         while (sc.hasNextLine()){
-        int =
-
+        dict.add(sc.nextLine());
         }
-
-
-
-
-
         return dict;
     }
 
