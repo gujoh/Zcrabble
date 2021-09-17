f3a825f863e90873d1031c994fe0db4a55f6cb32
Ole <ole.fjeldsaa@live.com>
2021-09-17 11:51:00 +0200

    Folder cleanup

--- src/main/java/com/zcrabblers/zcrabble/Model/Dictionary.java
+++ src/main/java/com/zcrabblers/zcrabble/Model/Dictionary.java
@@ -1,5 +1,9 @@
 package com.zcrabblers.zcrabble.Model;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Dictionary {
 
     private static Dictionary instance;
@@ -11,4 +15,18 @@ public class Dictionary {
     public static Dictionary getInstance(){
         return instance;
     }
+
+    private ArrayList<String> dictArray(){
+        ArrayList<String> dict = new ArrayList<>();
+        File collins = new File("Collins Scrabble Words 2019");
+        Scanner sc = new Scanner(collins);
+        while ()
+
+
+
+
+
+        return dict;
+    }
 }
+
