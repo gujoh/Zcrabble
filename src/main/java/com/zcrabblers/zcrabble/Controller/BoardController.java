9f16225c6f565a44b17458e714cf0bd7f841cc63
gujoh <gustafjohanson@live.com>
2021-09-17 16:39:32 +0200

    Adding things to boardcontroller

--- src/main/java/com/zcrabblers/zcrabble/Controller/BoardController.java
+++ src/main/java/com/zcrabblers/zcrabble/Controller/BoardController.java
@@ -5,6 +5,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -14,8 +15,11 @@ import java.util.ResourceBundle;
 
 public class BoardController implements Initializable {
     @FXML private AnchorPane boardAnchor;
-    ArrayList<ImageView> cellList = new ArrayList<>();
+    @FXML private AnchorPane rackAnchor;
+    @FXML private Rectangle rackRectangle;
 
+    ArrayList<ImageView> cellList = new ArrayList<>();
+    ArrayList<ImageView> rackList = new ArrayList<>();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -27,11 +31,18 @@ public class BoardController implements Initializable {
     }
 
     public void populate() throws FileNotFoundException {
+        populateBoard();
+        populateRack();
+
+    }
+
+    private void populateBoard() throws FileNotFoundException {
         int x = 0;
         int y = 0;
         for(int i = 1; i <= 225; i++){
             ImageView img = new ImageView();
             boardAnchor.getChildren().add(img);
+            cellList.add(img);
             img.setFitHeight(33);
             img.setFitWidth(33);
             img.setX(x);
@@ -41,10 +52,31 @@ public class BoardController implements Initializable {
                 x = 0;
                 y+=33;
             }
-            cellList.add(new ImageView());
             img.setImage((new Image(new FileInputStream("src/main/resources/com/zcrabblers/zcrabble/Images/BasicCell.png"))));
-
         }
+    }
 
+    private void populateRack() throws FileNotFoundException {
+        double x = rackRectangle.getWidth()/2-((double)33/2);
+        double y = rackRectangle.getHeight()/2-((double)33/2);
+        int counter = 0;
+        for(int i = 1; i <= 8; i++){
+            ImageView img = new ImageView();
+            rackAnchor.getChildren().add(img);
+            rackList.add(img);
+            img.setFitHeight(33);
+            img.setFitWidth(33);
+
+            img.setX(x);
+            img.setY(y);
+
+            if (counter % 2 == 0){
+                x += ((-counter)*45);
+            }
+            else x += (counter*45);
+            counter++;
+
+            img.setImage((new Image(new FileInputStream("src/main/resources/com/zcrabblers/zcrabble/Images/BasicCell.png"))));
+        }
     }
 }
