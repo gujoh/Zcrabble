81f66da1a471babd1ed38a923061d124e81a17db
gujoh <gustafjohanson@live.com>
2021-09-17 15:43:10 +0200

    Setting up fxml file

--- src/main/java/com/zcrabblers/zcrabble/Controller/BoardController.java
+++ src/main/java/com/zcrabblers/zcrabble/Controller/BoardController.java
@@ -1,19 +1,21 @@
 package com.zcrabblers.zcrabble.Controller;
 
-import com.zcrabblers.zcrabble.HelloApplication;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class BoardController implements Initializable {
+    @FXML private AnchorPane boardAnchor;
+    ArrayList<ImageView> cellList = new ArrayList<>();
+
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -23,7 +25,26 @@ public class BoardController implements Initializable {
             e.printStackTrace();
         }
     }
+
     public void populate() throws FileNotFoundException {
-        //image.setImage(new Image(new FileInputStream("src/main/resources/com/zcrabblers/zcrabble/Images/aircraft1.jpg")));
+        int x = 0;
+        int y = 0;
+        for(int i = 1; i <= 225; i++){
+            ImageView img = new ImageView();
+            boardAnchor.getChildren().add(img);
+            img.setFitHeight(33);
+            img.setFitWidth(33);
+            img.setX(x);
+            img.setY(y);
+            x+=33;
+            if(i % 15 == 0){
+                x = 0;
+                y+=33;
+            }
+            cellList.add(new ImageView());
+            img.setImage((new Image(new FileInputStream("src/main/resources/com/zcrabblers/zcrabble/Images/BasicCell.png"))));
+
+        }
+
     }
 }
