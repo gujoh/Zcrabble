6e97bfc8f8dd92013b4447d46f17add144b70ca6
martin <martinbcd@live.se>
2021-09-21 12:32:14 +0200

    added small changes to the datastructure to fit with board

--- src/main/java/com/zcrabblers/zcrabble/Model/Cell.java
+++ src/main/java/com/zcrabblers/zcrabble/Model/Cell.java
@@ -6,20 +6,16 @@ public class Cell {
     private Tile placedTile;
 
 
-    public Cell(int cellWordMultiplier,int cellLettermultiplier, Tile placedTile) {
-        this.cellWordMultiplier = this.cellWordMultiplier;
-        this.cellLetterMultiplier = this.cellWordMultiplier;
+    public Cell(int cellWordMultiplier,int cellLetterMultiplier, Tile placedTile) {
+        this.cellWordMultiplier = cellWordMultiplier;
+        this.cellLetterMultiplier = cellLetterMultiplier;
         this.placedTile = placedTile;
     }
-    // testing cell
-    public Cell(Cell cell){
-        this.cellWordMultiplier = cell.cellWordMultiplier;
-        this.cellLetterMultiplier = this.cellWordMultiplier;
-        this.placedTile = new Tile(cell.placedTile);
-    }
-
-    public int GetCellScore() {
+    public int GetCellWordMultiplier() {
         return cellWordMultiplier;
+    }
+    public int GetCellLetterMultiplier() {
+        return cellLetterMultiplier;
     }
 
     private int GetCombinedCellScore() {
