d1d4038ad4e730fdb73126575b74eed550303860
Ole <ole.fjeldsaa@live.com>
2021-09-21 10:38:21 +0200

    cleanup in Dictionary

--- src/main/java/com/zcrabblers/zcrabble/Model/Dictionary.java
+++ src/main/java/com/zcrabblers/zcrabble/Model/Dictionary.java
@@ -28,20 +28,5 @@ public class Dictionary {
         return false;
     }
 
-    /*
-
-    Method to create a faster dictionary.will be implemented later.
-
-    private ArrayList<String> dictArray() throws FileNotFoundException {
-        ArrayList<String> dict = new ArrayList<>();
-        File collins = new File("Collins Scrabble Words 2019");
-        Scanner sc = new Scanner(collins);
-        while (sc.hasNextLine()){
-        dict.add(sc.nextLine());
-        }
-        return dict;
-    }
-
-     */
 }
 
