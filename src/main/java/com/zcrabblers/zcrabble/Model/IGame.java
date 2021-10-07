dcd037b33719e52649141b38521701d2278d7919
gujoh <gustafjohanson@live.com>
2021-10-07 16:24:48 +0200

    Made a draft for the endTurn() method in Game and updated the observer pattern.

--- src/main/java/com/zcrabblers/zcrabble/Model/IGame.java
+++ src/main/java/com/zcrabblers/zcrabble/Model/IGame.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 public interface IGame {
-    void start();
     void addSubscriber(ILetterObservable sub);
     void removeSubscriber(ILetterObservable sub);
     void removeAllSubscribers();
--- src/main/java/com/zcrabblers/zcrabble/Model/ILetterObservable.java
+++ src/main/java/com/zcrabblers/zcrabble/Model/ILetterObservable.java
@@ -1,6 +1,8 @@
 package com.zcrabblers.zcrabble.Model;
 
+import java.util.ArrayList;
+
 // Implemented by classes who wish to subscribe to a LetterObserver
 public interface ILetterObservable {
-    void update(LetterTuple[] boardList, LetterTuple[] rackList);
+    void update(ArrayList<LetterTuple> boardList, ArrayList<LetterTuple> rackList);
 }
