285b84bc77fed1d6da011acbbb27ba1d5099a0fe
niklas axelsson <niklas.axelsson94@gmail.com>
2021-10-07 11:59:03 +0200

    Started on click/place behaviour

--- src/main/java/com/zcrabblers/zcrabble/Controller/Selection.java
+++ src/main/java/com/zcrabblers/zcrabble/Controller/Selection.java
@@ -4,9 +4,27 @@ import javafx.scene.image.Image;
 
 public class Selection {
     private CellView selectedCell;
+    private boolean fromRack;
 
+    private int startX, startY;
     public Selection(){
         //TODO: set selected cell to some kind of non CellView
+    }
+
+    public void changeToDefaultImage(){
+        selectedCell.changeToDefaultImage();
+    }
+
+    public void setImage(Image image){
+        selectedCell.setImage(image);
+    }
+
+    public boolean getFromRack(){
+        return fromRack;
+    }
+
+    public void setFromRack(boolean fromRack){
+        this.fromRack = fromRack;
     }
 
     public boolean hasSelected(){
@@ -31,5 +49,21 @@ public class Selection {
 
     public void unSelect(){
         selectedCell = null; //TODO: avoid null
+    }
+
+    public int getStartX() {
+        return startX;
+    }
+
+    public void setStartX(int startX) {
+        this.startX = startX;
+    }
+
+    public int getStartY() {
+        return startY;
+    }
+
+    public void setStartY(int startY) {
+        this.startY = startY;
     }
 }
