393058a070eb26aafb347fb6ebadb676872d8c82
martin <martinbcd@live.se>
2021-10-08 11:06:06 +0200

    added to cell tests

--- src/main/java/com/zcrabblers/zcrabble/Tests/CellTests.java
+++ src/main/java/com/zcrabblers/zcrabble/Tests/CellTests.java
@@ -1,4 +1,29 @@
 package com.zcrabblers.zcrabble.Tests;
 
+import com.zcrabblers.zcrabble.Model.Cell;
+import com.zcrabblers.zcrabble.Model.Tile;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CellTests {
+
+    @Test
+    public void TestConstructor(){
+        Cell cell = new Cell(4 , 5, new Tile(' ', 0));
+        assertEquals(4, cell.GetCellWordMultiplier());
+        assertEquals(5, cell.GetCellLetterMultiplier());
+    }
+    @Test
+    public void TestFunctions(){
+        Cell cell = new Cell(6 , 8, new Tile('A', 0));
+        assertEquals(0, cell.getPlacedTile().getTileScore());
+        assertEquals('A', cell.getPlacedTile().getLetter());
+        cell.setTile(new Tile('B',1));
+        assertEquals(1, cell.getPlacedTile().getTileScore());
+        assertEquals('B', cell.getPlacedTile().getLetter());
+        cell.removeTile();
+        assertEquals(0, cell.getPlacedTile().getTileScore());
+        assertEquals(' ', cell.getPlacedTile().getLetter());
+        assertTrue(cell.isEmpty());
+    }
 }
