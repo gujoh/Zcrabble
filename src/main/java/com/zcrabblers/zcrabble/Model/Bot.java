351f9ee23a19ebcbf7596a7b43bb3b48e0624c0a
Ole <ole.fjeldsaa@live.com>
2021-10-08 10:48:26 +0200

    Created method scrabbleWord in bot, it finds possible words to place out based on the board Needs support methods.

--- src/main/java/com/zcrabblers/zcrabble/Model/Bot.java
+++ src/main/java/com/zcrabblers/zcrabble/Model/Bot.java
@@ -1,6 +1,7 @@
 package com.zcrabblers.zcrabble.Model;
 
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -69,83 +70,137 @@ public class Bot implements IPlayers {
     D   {  ,  ,  ,  ,  , X, X, X, X, X, X, X,  ,  ,  },
     E   {  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  },
 
+     */
 
+
+    private static Cell[][] scrabbleWord(Cell[][] board, String rackString){
+        int rows = board.length;
+        int cols = board[0].length;
+        Cell[][] currentWord = new Cell[rows][cols];
+        Cell[][] bestWord = new Cell[rows][cols];
+        int spaceBehind;
+        int spaceAhead;
+        ArrayList<String> writable;
+        StringBuilder letters = new StringBuilder();
+        StringBuilder tempRack = new StringBuilder(rackString);
+        char [] wordSpace;  //Should this be Cell[], we will se I guess.
+
+        for (int row = 0; row <board.length ; row++) {
+            for (int col = 0; col <board[0].length ; col++) {
+
+                searchForLetters(board,letters,tempRack,row,col);
+
+                spaceBehind = checkSpaceBehind(board, row, col-letters.length());
+                spaceAhead = checkSpaceAhead(board, row, col);
+                wordSpace = createWordSpace(col-letters.length(),spaceBehind,spaceAhead,letters);
+                writable = actuallyWritable(wordSpace,tempRack.toString(),spaceBehind,spaceAhead,letters.toString());
+
+            }
+            
         }
-        writeAHorizontalWord(Board board){
+    return bestWord;
+    }
 
-        StringBuilder rackAndFriends = new StringBuilder;
-        rackAndFriends.append(rack)
-        ArrayList<String> writableWords = new Arraylist;
+    private static ArrayList<String> actuallyWritable(char[] wordSpace, String toString, int spaceBehind, int spaceAhead, String toString1) {
+        return null;
+    }
 
-        for (row:Rows)
-            for(col:Columns)
-                if(containsLetter(board,row,col) &&  checkInterval(board,row,col,rack.Size)) //if a cell has a letterTile and space around it
-                   rackAndFriends.append(tileLetter)
-                   writableWords = canWrite(rackAndFriends);
-                   place down the first word that can be written
+    private static char[] createWordSpace(int i, int spaceBehind, int spaceAhead, StringBuilder letters) {
+        return null;
+    }
 
+    private static int checkSpaceAhead(Cell[][] board, int row, int col) {
+        return 0;
+    }
 
+    private static int checkSpaceBehind(Cell[][] board, int row, int startCol) {
+        int space = 0;
+        for (int k = startCol-1; k >= 0; k--) {
+            if (!board[row][k].isEmpty()){
+                space -= 1;
+                break;
+            }else space += 1;
         }
+        return space;
+    }
 
 
+/*
+                    spaceBehind = checkSpaceBehind(tempBoard,i,startOfWord);
+                    spaceAhead = checkSpaceAhead(tempBoard,i,j);
+                    wordSpace = createWordSpace(startOfWord,spaceBehind,spaceAhead,letters);
+                    writable = actuallyWritable(wordSpace,tempRack.toString(),spaceBehind,spaceAhead,letters.toString());
 
 
+    /*
 
-
-
-
-
-
-
-     checkInterval(Board board,Int row,Int col,Int interval){
-           for(col==col,col <=interval, col
-
-
-
-     }
-
-
-
-
+                    writeToTempBoard(tempBoard, writable, i, j, letters.toString(), spaceBehind);
+                    print2D(tempBoard);
+                }
+                letters.delete(0,letters.length());
+                tempRack = new StringBuilder(rack);
+                wordSpace = new char[0];
+                tempBoard = boardCopy(board);
+            }
+        }
+        return tempBoard;
+    }
 
 
      */
 
+    private static void searchForLetters(Cell[][] board, StringBuilder letters, StringBuilder tempRack, int i, int j) {
 
+        if (!board[i][j].isEmpty()) {
+            tempRack.append(board[i][j].getTileLetter());
+            letters.append(board[i][j].getTileLetter());
 
-
-    /*---   Takes in a string of letters and returns all dictionary words that can be written with them   ---*/
-    private static ArrayList<String> canWrite (String letters){
-        ArrayList <String> writableWords = new ArrayList<>();
-        Map<Character,Integer> charCountMap = getCharCountMap(letters);
-        for (String s : dict.getDictArray()) {
-            Map<Character, Integer> currentDictWordMap = getCharCountMap(s);
-            boolean canMakeWord = true;
-            for (Character character : currentDictWordMap.keySet()) {
-                int currentDictWordMapCharCount = currentDictWordMap.get(character);
-                int lettersCharCount = charCountMap.getOrDefault(character, 0);
-                if (currentDictWordMapCharCount > lettersCharCount) {
-                    canMakeWord = false;
+            //TODO make this loop better, while j != cols-1 && !j+1.isEmpty
+            while (!board[i][j == board[0].length - 1 ? j : j + 1].isEmpty()) {
+                if (j == board[0].length - 1) {
                     break;
                 }
-            }
-            if (canMakeWord){
-                writableWords.add(s);
+                j++;
+                tempRack.append(board[i][j].getTileLetter());
+                letters.append(board[i][j].getTileLetter());
             }
         }
-        return writableWords;
     }
 
-    /*---   Takes in a string of letters and returns HashMap with its Characters as keys and number of times it is used as value   ---*/
-    private static Map<Character,Integer> getCharCountMap(String letters){
-        Map<Character, Integer> charCountMap = new HashMap<>();
-        for (int i = 0; i <letters.length() ; i++) {
+    /*--- Don't touch things below this line ---*
 
-            char currentChar = letters.charAt(i);
-            int count = charCountMap.getOrDefault(currentChar, 0);
-            charCountMap.put(currentChar,count+1);
+    /*---   Takes in a string of letters and returns all dictionary words that can be written with them   ---*/
+        private static ArrayList<String> canWrite (String letters){
+            ArrayList<String> writableWords = new ArrayList<>();
+            Map<Character, Integer> charCountMap = getCharCountMap(letters);
+            for (String s : dict.getDictArray()) {
+                Map<Character, Integer> currentDictWordMap = getCharCountMap(s);
+                boolean canMakeWord = true;
+                for (Character character : currentDictWordMap.keySet()) {
+                    int currentDictWordMapCharCount = currentDictWordMap.get(character);
+                    int lettersCharCount = charCountMap.getOrDefault(character, 0);
+                    if (currentDictWordMapCharCount > lettersCharCount) {
+                        canMakeWord = false;
+                        break;
+                    }
+                }
+                if (canMakeWord) {
+                    writableWords.add(s);
+                }
+            }
+            return writableWords;
         }
-        return charCountMap;
-    }
 
-}
+        /*---   Takes in a string of letters and returns HashMap with its Characters as keys and number of times it is used as value   ---*/
+        private static Map<Character, Integer> getCharCountMap (String letters){
+            Map<Character, Integer> charCountMap = new HashMap<>();
+            for (int i = 0; i < letters.length(); i++) {
+
+                char currentChar = letters.charAt(i);
+                int count = charCountMap.getOrDefault(currentChar, 0);
+                charCountMap.put(currentChar, count + 1);
+            }
+            return charCountMap;
+        }
+
+    }
