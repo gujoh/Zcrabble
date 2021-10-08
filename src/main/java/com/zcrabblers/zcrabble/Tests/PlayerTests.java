ee0355016e87c1fdb3a082689029a740eb1d3a0f
martin <martinbcd@live.se>
2021-10-08 12:45:37 +0200

    added basic tests for code coverage

--- src/main/java/com/zcrabblers/zcrabble/Tests/PlayerTests.java
+++ src/main/java/com/zcrabblers/zcrabble/Tests/PlayerTests.java
@@ -1,4 +1,30 @@
 package com.zcrabblers.zcrabble.Tests;
 
+import com.zcrabblers.zcrabble.Model.Player;
+import com.zcrabblers.zcrabble.Model.Rack;
+import com.zcrabblers.zcrabble.Model.Tile;
+import com.zcrabblers.zcrabble.Model.TileBag;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class PlayerTests {
+
+    @Test
+    public void TestSwitchRack() throws FileNotFoundException {
+        TileBag tilebag = new TileBag("defaultBag");
+        tilebag.selectBag();
+        Rack rack = new Rack(tilebag);
+        Player player = new Player(0, rack);
+        player.addScore(20);
+        assertEquals(20,player.getScore());
+        assertEquals(rack.getTile(1), player.getRackTile(1));
+        Tile tile1 = new Tile('A', 2);
+        player.placeRackTile(1,tile1);
+        assertEquals(tile1,player.getRackTile(1));
+
+    }
 }
--- src/main/java/com/zcrabblers/zcrabble/Tests/RackTests.java
+++ src/main/java/com/zcrabblers/zcrabble/Tests/RackTests.java
@@ -1,4 +1,37 @@
 package com.zcrabblers.zcrabble.Tests;
 
+import com.zcrabblers.zcrabble.Model.Rack;
+import com.zcrabblers.zcrabble.Model.Tile;
+import com.zcrabblers.zcrabble.Model.TileBag;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+
 public class RackTests {
+
+    @Test
+    public void TestSwitchRack() throws FileNotFoundException {
+        TileBag tilebag = new TileBag("defaultBag");
+        tilebag.selectBag();
+        Rack rack = new Rack(tilebag);
+        Tile tile1 = rack.getTile(1);
+        Tile tile2 = rack.getTile(2);
+        rack.switchTiles(1,2);
+        assertEquals(tile1, rack.getTile(2));
+        assertEquals(tile2, rack.getTile(1));
+
+    }
+    @Test
+    public void TestRack() throws FileNotFoundException {
+        TileBag tilebag = new TileBag("defaultBag");
+        tilebag.selectBag();
+        Rack rack = new Rack(tilebag);
+        Tile tile1 = rack.getTile(1);
+        Tile tile2 = rack.getTile(2);
+        rack.remove(1);
+        assertTrue(rack.isEmpty(1));
+        rack.set(1, new Tile('A',2));
+
+    }
 }
