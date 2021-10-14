39ea6181df6130103369c86b5accbf4d2f3ff69b
Ole <ole.fjeldsaa@live.com>
2021-10-14 10:43:27 +0200

    changes to endTurn

--- src/main/java/com/zcrabblers/zcrabble/Model/Game.java
+++ src/main/java/com/zcrabblers/zcrabble/Model/Game.java
@@ -45,12 +45,13 @@ public class Game implements IGame, ITurnObservable {
 
     @Override
     public boolean endTurn(){ // DISCUSS THIS.
-        if (tempBoard.checkBoard(tempBoard)){
-            current.addScore(tempBoard.countPoints(tempBoard.getNewCells(board)));
+        if (tempBoard.checkBoard(tempBoard, board)){
+            current.addScore(tempBoard.countPoints(tempBoard.getNewCells(board)));  //TODO addScore should make it's own cellTuple, getNewCells is in the same class.
             current.fillRack(tileBag);
             current = getNextPlayer();
+            observer.notifySubscribers((ArrayList<CellTuple>) tempBoard.getNewCells(board));
+            System.out.println(tempBoard.getNewCells(board).size());
             board.copyBoardCells(tempBoard);
-            observer.notifySubscribers(boardList);
             boardList.clear();
             current.beginTurn(tempBoard);
             System.out.println(players.indexOf(current));
