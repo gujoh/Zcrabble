113323262c5834e7e7966f0460b14ebf09a9307a
niklas axelsson <niklas.axelsson94@gmail.com>
2021-10-24 19:07:15 +0200

    Comments and updated design model

--- src/main/java/com/zcrabblers/zcrabble/model/gameBoard/EmptyTile.java
+++ src/main/java/com/zcrabblers/zcrabble/model/gameBoard/EmptyTile.java
@@ -9,9 +9,20 @@ package com.zcrabblers.zcrabble.model.gameBoard;
 enum EmptyTile {
     INSTANCE;
     Tile empty = new Tile(' ', 0);
+
+    /**
+     * Gets the empty tile.
+     * @return A reference to the empty tile.
+     */
     public Tile getEmpty(){
         return empty;
     }
+
+    /**
+     * Check to see if a tile is the empty tile.
+     * @param tile Tile to compare.
+     * @return True if tile is the empty tile, false otherwise.
+     */
     public boolean isEmpty(Tile tile){
         return tile == empty;
     }
